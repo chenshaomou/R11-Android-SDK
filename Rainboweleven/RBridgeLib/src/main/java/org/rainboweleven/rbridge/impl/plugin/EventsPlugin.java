e70fbcd0199b1b70e15b922151ed25482857efe9
andy <wlfcolin@hotmail.com>
2018-03-25 21:17:04 +0800

    优化EventsPlugin代码

--- Rainboweleven/RBridgeLib/src/main/java/org/rainboweleven/rbridge/impl/plugin/EventsPlugin.java
+++ Rainboweleven/RBridgeLib/src/main/java/org/rainboweleven/rbridge/impl/plugin/EventsPlugin.java
@@ -1,14 +1,11 @@
 package org.rainboweleven.rbridge.impl.plugin;
 
+import android.content.Context;
+
 import org.json.JSONObject;
 import org.rainboweleven.rbridge.core.RPromise;
-import org.rainboweleven.rbridge.core.RWebViewInterface.EventObserver;
 import org.rainboweleven.rbridge.core.RWebkitPlugin;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.rainboweleven.rbridge.impl.REventsCenter;
 
 /**
  * Created by chenshaomou on 24/03/2018.
@@ -18,10 +15,10 @@ public class EventsPlugin extends RWebkitPlugin {
     public static final String MODULE_NAME = "events";
     public static final String SEND_EVENT = "send";
 
-    private Map<String, List<EventObserver>> mEventObserversMap;
+    private Context mContext;
 
-    public EventsPlugin() {
-        mEventObserversMap = new HashMap<>();
+    public EventsPlugin(Context context) {
+        mContext = context;
     }
 
     @Override
@@ -36,49 +33,13 @@ public class EventsPlugin extends RWebkitPlugin {
                 String eventName = jsonParams.optString("eventName");
                 String eventParams = jsonParams.optString("params");
 
-                List<EventObserver> observers = mEventObserversMap.get(eventName);
-                if (observers != null) {
-                    for (EventObserver observer : observers) {
-                        if (observer == null) {
-                            continue;
-                        }
-                        observer.onObserver(eventName, eventParams);
-                    }
-                    promise.setResult("true");// 事件发送成功
-                }
+                REventsCenter.getInstance(mContext).sendByH5(eventName, eventParams);
+                promise.setResult("true");// 事件发送成功
+
             } catch (Exception e) {
                 e.printStackTrace();
                 promise.setResult(e.getMessage());// 事件发送失败
             }
         }
     }
-
-    /**
-     * 添加观察者
-     *
-     * @param eventName
-     * @param observer
-     */
-    public void addObserver(String eventName, EventObserver observer) {
-        List<EventObserver> observers = mEventObserversMap.get(eventName);
-        if (observers == null) {
-            observers = new ArrayList<>();
-            mEventObserversMap.put(eventName, observers);
-        }
-        observers.add(observer);
-    }
-
-    /**
-     * 移除观察者
-     *
-     * @param eventName
-     * @param observer
-     */
-    public void removeObserver(String eventName, EventObserver observer) {
-        List<EventObserver> observers = mEventObserversMap.get(eventName);
-        if (observers != null) {
-            observers.remove(observer);
-        }
-    }
-
 }
